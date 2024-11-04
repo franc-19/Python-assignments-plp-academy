8f816033158d869810c6345faf2ca87d253a8cb3
franc-19 <133057947+franc-19@users.noreply.github.com>
2024-11-04 15:28:55 +0300

    completed

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name = input("What is your name? ")
+age = input("What is your age ")
+location = input("Where do live? ")
+
+print("Hello {}, you are {} years old and live in {}.".format(name, age, location))
